aa964ea6b05e7e669ccaa9dc0d77eeb129a69b67
TAG-Epic <admin@itzepic.net>
2020-08-17 16:38:08 +0200

    Feat: add instructions for how to install it

---
+++ README.md
@@ -0,0 +1,7 @@
+# Salbot v3 bundle
+
+## How to install
+- Install docker and docker-compose
+- Update the config files in the config directory
+- Start the database up (This is only required first time): `docker-compose up database`
+- Start up salbot: `docker-compose up -d`. You could optionally scale MAX
--- config/bot-config.py
+++ config/bot-config.py
@@ -2,14 +2,14 @@
 Created by Epic at 8/17/20
 """
 
-TOKEN = ""
-WEBHOOK_URL = ""
-MAX_URL = "max:5000"  # https://github.com/IBM/MAX-Toxic-Comment-Classifier
-SENTRY_URL = ""
+TOKEN = ""  # Discord bot token
+WEBHOOK_URL = ""  # Discord webhook url to put logs into
+MAX_URL = "max:5000"  # URL to MAX, do NOT change
+SENTRY_URL = ""  # Sentry dsn
 
 # API
-API_URL = "api:5000"
-API_TOKEN = ""
+API_URL = "api:5000"  # URL to salbot api, do NOT change
+API_TOKEN = ""  # Password for the api, this much match on the api and the bot.
 
 # Permissions
-DEVELOPER_IDS = []
+DEVELOPER_IDS = []  # A list of developers of the bot which has debug permissions
