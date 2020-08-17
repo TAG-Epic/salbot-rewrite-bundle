7020f3796823e5db71ca711d409297bb0fdd8a3e
TAG-Epic <admin@itzepic.net>
2020-08-17 16:14:37 +0200

    Feat: initial commit

---
+++ config/bot-config.py
@@ -0,0 +1,15 @@
+"""
+Created by Epic at 8/17/20
+"""
+
+TOKEN = ""
+WEBHOOK_URL = ""
+MAX_URL = "max:5000"  # https://github.com/IBM/MAX-Toxic-Comment-Classifier
+SENTRY_URL = ""
+
+# API
+API_URL = "api:5000"
+API_TOKEN = ""
+
+# Permissions
+DEVELOPER_IDS = []
